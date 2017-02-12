cb55a544d6cf1c0a506db581c113597024f54eb1
Joshua Shields <joshua.shields@DEV114.texturallc.net>
2017-02-12 17:53:21 -0600

    adjust canvas width and prepare for glasses sizing

--- js/dogglasses.js
+++ js/dogglasses.js
@@ -23,6 +23,7 @@ var ImageObject = function (image, transform) {
             this.image,
             this.transform.coords.x,
             this.transform.coords.y
+            // TODO
             //this.transform.scale * this.image.width,
             //this.transform.scale * this.image.height,
         );
@@ -43,16 +44,17 @@ addEventListener('mouseup', function (ev) {
     clicked = false;
 });
 
+// FIXME: window.innerWidth includes width of vertical scrollbar (15 in Chrome), we don't want that
+var resolution = new Coords(window.innerWidth - 15, window.innerHeight);
 
-var resolution = new Coords(640, 640);
 var canvas = document.createElement('canvas');
 canvas.innerText = 'Your browser does not support the canvas element.';
 canvas.width = resolution.x;
 canvas.height = resolution.y;
 var ctx = canvas.getContext('2d', {'alpha': false});
 
-window.onload = function () {
-    document.body.appendChild(canvas);
+window.onload = function (ev) {
+    document.getElementById('dogContainer').appendChild(canvas);
 
     // dog buttons
     document.getElementById('dogFile').addEventListener('change', function (ev) {
@@ -74,12 +76,10 @@ window.onload = function () {
 
         var glassesImg = new Image();
         glassesImg.src = src;
-        setGlasses(glassesImg);
+        setGlasses(glassesImgPath);
     });
     document.getElementById('defaultGlassesBtn').addEventListener('click', function (ev) {
-        var glassesImg = new Image();
-        glassesImg.src = 'https://raw.githubusercontent.com/jshields/dogglasses.io/master/img/dealwithit_glasses_front.png';
-        setGlasses(glassesImg);
+        setGlasses('https://raw.githubusercontent.com/jshields/dogglasses.io/master/img/dealwithit_glasses_front.png');
     });
 };
 
@@ -92,10 +92,6 @@ var init = function () {
     ctx.textAlign = 'left';
     ctx.textBaseline = 'top';
     ctx.fillText('Pick a dog to get started', 16, 16);
-
-
-    //
-    //ctx.drawImage(test, 0, 0);
 };
 
 var dog;
@@ -104,23 +100,28 @@ var glasses;
 var setDog = function (dogImgPath) {
     var dogImg = new Image();
     dogImg.src = dogImgPath;
+    dogImg.width = canvas.width;
 
     dogImg.addEventListener('load', function (ev) {
         dog = dogImg;
-        canvas.width = dogImg.width;
-        canvas.height = dogImg.height;
+
+        //canvas.width = dogImg.width;
+        //canvas.height = dogImg.height;
         // enable glasses picker
         document.getElementById('defaultGlassesBtn').removeAttribute('disabled');
         // start render loop
         main();
     });
 };
-var setGlasses = function (glassesImg) {
-    glassesImg.onload = function() {
-        var transform = new Transform(new Coords());
+var setGlasses = function (glassesImgPath) {
+    var glassesImg = new Image();
+    glassesImg.src = glassesImgPath;
+
+    glassesImg.addEventListener('load', function (ev) {
+        var transform = new Transform(new Coords(0, 0));
         var glassesObj = new ImageObject(glassesImg, transform);
         glasses = glassesObj;
-    };
+    });
 };
 
 var update = function () {
@@ -134,11 +135,13 @@ var update = function () {
 var render = function () {
     // draw dog as a background
     if (dog) {
-        ctx.drawImage(dog, 0, 0);
+        ctx.drawImage(dog, 0, 0, dog.width, dog.height);
     }
     // draw glasses
     if (glasses) {
         glasses.draw(ctx);
+    } else {
+        ctx.fillText('Now pick the glasses', 16, 16);
     }
 };
 
